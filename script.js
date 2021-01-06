4cd0d6d93ce7c48261f92f2eab9fcdb91dbdd64a
Tareeq Hamilton <tareeqhamilton@gmail.com>
2021-01-06 16:35:24 -0500

    added html framework and ajax call

---
+++ script.js
@@ -0,0 +1,12 @@
+// add jquery
+//funciton ajax call 
+// "htps:" + variable + "gadjkgabkj"  vs `https://${variable}khgfhjfg`
+
+// function forecastWeater(searchCity) {
+//  ajax
+// .addClass(“”).text(“the text”)
+
+var apiKey = "c40b28aa33c2bef2881ab9e4f13c3ef7";
+
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=c40b28aa33c2bef2881ab9e4f13c3ef7
